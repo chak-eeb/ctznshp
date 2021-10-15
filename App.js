8a803f951639042aef1f09d90d554f760c45a7fa
chakib <chak_eeb@yahoo.com>
2021-10-15 12:49:13 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>Hello there</Text>
+    </View>
+  );
+};
+
+export default App;
