f4268f87d5746f07428574eb8a3c1931c064efe8
chakib <chak_eeb@yahoo.com>
2021-10-15 13:04:23 -0400

    configure firebase

---
+++ database/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAWI2pTxPgnjtFWtpnd6JEDxu59SoaVThM',
+  authDomain: 'ctznshp-69e09.firebaseapp.com',
+  projectId: 'ctznshp-69e09',
+  storageBucket: 'ctznshp-69e09.appspot.com',
+  messagingSenderId: '189164841957',
+  appId: '1:189164841957:web:ee3ebb10d54132ad1b1d0c',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
